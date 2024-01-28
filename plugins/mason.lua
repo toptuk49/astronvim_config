eb3b59b2bcbdfd1085c24973865d34e8cfc52f1d
toptuk49 <chyka228tigra@yandex.ru>
2024-01-28 17:52:33 +0300

    Setted up for my needs

--- plugins/mason.lua
+++ plugins/mason.lua
@@ -19,7 +19,7 @@ return {
       -- add more things to the ensure_installed table protecting against community packs modifying it
       opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
         -- "prettier",
-        -- "stylua",
+        "stylua",
       })
     end,
   },
